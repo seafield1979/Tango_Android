5e30f912357ff9e4d32f47a6c36efb9b14b7c4c6
shusuke unno <seafield1979@gmail.com>
2016-10-24 15:30:42 +0900

    単語帳アプリ

---
+++ TangoBook/app/src/main/java/com/sunsunsoft/shutaro/tangobook/MainActivity.java
@@ -0,0 +1,84 @@
+package com.sunsunsoft.shutaro.tangobook;
+
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setViews();
+    }
+
+    private void setViews() {
+        // toolbar
+        mToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(mToolbar);
+
+        // view pager
+        FragmentManager manager = getSupportFragmentManager();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.main_viewpager);
+        MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(manager);
+        viewPager.setAdapter(adapter);
+
+        // tab に view pager のページのタイトルを表示
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.main_tab);
+        tabLayout.setupWithViewPager(viewPager);
+
+        // drawer
+        setDrawer();
+
+    }
+
+    /**
+     * Drawerをセットアップ
+     */
+    private void setDrawer() {
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.main_drawer_navigation);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, mDrawerLayout, mToolbar, R.string.app_name, R.string.app_name);
+        mDrawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(select);
+    }
+
+    private NavigationView.OnNavigationItemSelectedListener select =
+            new NavigationView.OnNavigationItemSelectedListener()
+    {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.menu_manage:
+                    break;
+                case R.id.menu_gallery:
+                    break;
+                case R.id.menu_alert:
+                    break;
+                case R.id.menu_save:
+                    break;
+                case R.id.menu_setting:
+                    break;
+                default:
+                    // ドロワーを閉じる
+                    mDrawerLayout.closeDrawers();
+            }
+            return true;
+        }
+    };
+}
---
+++ TangoBook/app/src/main/java/com/sunsunsoft/shutaro/tangobook/NoScrollViewPager.java
@@ -0,0 +1,32 @@
+package com.sunsunsoft.shutaro.tangobook;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+
+/**
+ * スクロールしないViewPager
+ */
+public class NoScrollViewPager extends ViewPager {
+    public NoScrollViewPager(Context context) {
+        super(context);
+    }
+
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent arg0) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Never allow swiping to switch between pages
+        return false;
+    }
+}
