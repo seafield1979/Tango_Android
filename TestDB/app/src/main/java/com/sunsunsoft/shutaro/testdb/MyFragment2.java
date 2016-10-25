2f60e32d50f2987dc6d4b43e5b3dfa1ea2f4f3d7
shusuke unno <seafield1979@gmail.com>
2016-10-26 00:22:21 +0900

    単語帳のデータベーステスト用プロジェクト追加

---
+++ TestDB/app/src/main/java/com/sunsunsoft/shutaro/testdb/MyFragment2.java
@@ -0,0 +1,41 @@
+package com.sunsunsoft.shutaro.testdb;
+
+
+import android.os.Bundle;
+import android.support.annotation.ColorRes;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MyFragment2 extends Fragment {
+    private final static String BACKGROUND_COLOR = "background_color";
+
+    public static MyFragment2 newInstance(@ColorRes int IdRes) {
+        MyFragment2 frag = new MyFragment2();
+        Bundle b = new Bundle();
+        b.putInt(BACKGROUND_COLOR, IdRes);
+        frag.setArguments(b);
+        return frag;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page2, null);
+        LinearLayout linearLayout = (LinearLayout) view.findViewById(R.id.fragment_page2_linearlayout);
+        linearLayout.setBackgroundResource(getArguments().getInt(BACKGROUND_COLOR));
+
+        return view;
+    }
+}
---
+++ TestDB/app/src/main/java/com/sunsunsoft/shutaro/testdb/MyFragmentPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.sunsunsoft.shutaro.testdb;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by shutaro on 2016/10/19.
+ */
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+    public MyFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return MyFragment.newInstance(android.R.color.holo_blue_bright);
+            case 1:
+                return MyFragment2.newInstance(android.R.color.holo_green_light);
+            case 2:
+                return MyFragment3.newInstance(android.R.color.holo_red_dark);
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "ページ" + (position + 1);
+    }
+}
